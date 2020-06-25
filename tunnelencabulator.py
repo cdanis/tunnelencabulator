6d29d8c86496dcf742074f85222db0d85c4d386a
Chris Danis <cdanis@gmail.com>
2020-06-25 11:27:28 -0400

    yet more macos fixes

--- tunnelencabulator.py
+++ tunnelencabulator.py
@@ -219,7 +219,7 @@ def main(args):
                           "please brew install socat or sudo port install socat")
                     return
 
-                [subprocess.run(["/sbin/ifconfig", "lo0", "alias", ip, "up"])
+                [subprocess.run(["/usr/bin/sudo", "/sbin/ifconfig", "lo0", "alias", ip, "up"])
                  for ip in prefabulate_tunnel().values()]
 
                 replenerated_ports = {replenerate_hostname(h): p for (h, p) in TUNNEL_HOSTS.items()}
