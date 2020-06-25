302bc198b048f71b8cbed3f71a34ba5a2a00f414
Chris Danis <cdanis@gmail.com>
2020-06-25 01:58:22 -0400

    tweak phrasing

--- tunnelencabulator.py
+++ tunnelencabulator.py
@@ -3,7 +3,7 @@
 For a number of years now, work has been proceeding in order to bring to perfection the
 crudely-conceived idea of a machine that would not only supply the easy re-routing of
 traffic for load-balanced services, but would also be capable of automatically synchronizing
-directional LibreNMSes and icingas.  Such an instrument is the tunnel-encabulator.
+single-homed LibreNMSes and icingas.  Such an instrument is the tunnel-encabulator.
 """
 
 __author__ = "Chris Danis"
